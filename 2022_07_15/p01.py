a6c1e033b2511fca11dcce76b013f45a0d77bb26
张济东 <1436581890@qq.com>
2023-02-21 22:02:59 +0800

    2022_07_15

---
+++ 2022_07_15/p01.py
@@ -0,0 +1,16 @@
+"""
+ctrl + c 复制
+ctrl + v 粘贴
+ctrl + x 剪切
+ctrl + z 返回上一步
+ctrl + y 和ctrl + z相反
+ctrl + s 保存
+"""
+
+"""
+写代码换行需要光标移到最后,麻烦,可以ctrl + enter换行
+"""
+
+print(123456)
+print("Hello World")
+print(122)
---
+++ 2022_07_15/p02.py
@@ -0,0 +1,6 @@
+"""
+Python之禅(Zen of Python)
+小彩蛋,八荣八耻
+"""
+
+import this
