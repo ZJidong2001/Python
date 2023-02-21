21c86244bd99c0388ee2cfe8a61ae8e3c8ab981e
张济东 <1436581890@qq.com>
2023-02-22 00:13:32 +0800

    2022_07_20_homework

---
+++ 2022_07_20_homework/homework.py
@@ -0,0 +1,25 @@
+""" 不要借助Python IDE, 请手动作答 """
+
+# string = "hello world"，如何切片可以输出 "hello w" ？
+
+# string[0 : 7]
+
+# string = "hello world"，如何切片可以输出空格字符 ？
+
+# string[5 : 6]
+
+# string = "hello world"，如何切片可以输出 "d" ？
+
+# string[10:12]
+
+# string = "hello world"，string[: 6]的结果是什么 ？
+
+# "hello "
+
+# string = "hello world"，string[1 : 8 : 2]的结果是什么 ？
+
+# "el o"
+
+# string = "hello world"，string[-2 : 3 : -2]的结果是什么 ？
+
+# "lo "
